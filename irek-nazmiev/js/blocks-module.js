7248f70838b9835b4eaf49524b6a9b546236a3f0
Nagellan <31626879+Nagellan@users.noreply.github.com>
2018-06-13 20:29:37 +0300

    Add ability to delete blocks - trash can

--- irek-nazmiev/js/blocks-module.js
+++ irek-nazmiev/js/blocks-module.js
@@ -20,19 +20,39 @@ function addBlock(content) {
 }
 
 function moveBlock(block) {
-     block.onmousedown = function(e) {       // start moving on first click
-          var blockCoords = block.getBoundingClientRect();
-          var shiftX = e.pageX - blockCoords.left;
-          var shiftY = e.pageY - blockCoords.top;
 
-          document.onmousemove = function(e) {    // when mouse is moving, change
+     block.onmousedown = function(e) {       // start moving on first click
+          var x, y;
+
+          var blockCoords = block.getBoundingClientRect(),
+              shiftX = e.pageX - blockCoords.left,
+              shiftY = e.pageY - blockCoords.top;
+
+          block.style.zIndex = 1000;
+          document.getElementById("trash-can-wrapper").style.bottom = 0;
+
+          document.onmousemove = function(e) {    // when mouse is moving, change block's coordinates
                block.style.left = e.pageX - shiftX + 'px';
-               block.style.top = e.pageY - shiftY + 'px';                       // block's coordinates
+               block.style.top = e.pageY - shiftY + 'px';
+               x = e.pageX;
+               y = e.pageY;
           };
 
           block.onmouseup = function() {          // stop moving on second click
                document.onmousemove = null;
                block.onmouseup = null;
+               block.style.zIndex = "unset";
+               if (isInTrashCan(x, y))
+                    block.remove();
+               document.getElementById("trash-can-wrapper").style.bottom = "-15.7vh";
           };
+
+          function isInTrashCan(x, y){
+               var trashCanCoords = document.getElementById("trash-can").getBoundingClientRect();
+               return x >= trashCanCoords.left &
+                      x <= (trashCanCoords.left + trashCanCoords.width) &
+                      y >= trashCanCoords.top &
+                      y <= (trashCanCoords.top + trashCanCoords.height);
+          }
      }
 }
