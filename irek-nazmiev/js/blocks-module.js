a18e4253565fbaa957d92e184d83271848eb9e48
Nagellan <31626879+Nagellan@users.noreply.github.com>
2018-06-13 16:18:27 +0300

    Add ability to move blocks on a field

--- irek-nazmiev/js/blocks-module.js
+++ irek-nazmiev/js/blocks-module.js
@@ -13,5 +13,26 @@ function addBlock(content) {
      var block = document.createElement("button");
      block.className = "block";
      block.innerHTML = content;
+     block.style.top = field.clientHeight / 2 + "px";
+     block.style.left = field.clientWidth / 2 + "px";
+     block.setAttribute("onmousemove", "moveBlock(this);");
      field.appendChild(block);
+}
+
+function moveBlock(block) {
+     block.onmousedown = function(e) {       // start moving on first click
+          var blockCoords = block.getBoundingClientRect();
+          var shiftX = e.pageX - blockCoords.left;
+          var shiftY = e.pageY - blockCoords.top;
+
+          document.onmousemove = function(e) {    // when mouse is moving, change
+               block.style.left = e.pageX - shiftX + 'px';
+               block.style.top = e.pageY - shiftY + 'px';                       // block's coordinates
+          };
+
+          block.onmouseup = function() {          // stop moving on second click
+               document.onmousemove = null;
+               block.onmouseup = null;
+          };
+     }
 }
