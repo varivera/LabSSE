8e7de93d82f8fe09d72e39f3760d7c731139bdda
Nagellan <31626879+Nagellan@users.noreply.github.com>
2018-06-16 18:46:05 +0300

    Add ability to draw connections Yet, they're not updated dunamically while moving blocks and cannot be removed

--- irek-nazmiev/js/blocks-module.js
+++ irek-nazmiev/js/blocks-module.js
@@ -9,14 +9,6 @@ function openCloseSpoiler(block) {
      }
 }
 
-function addBlock(content) {
-     var block = document.createElement("button");
-     block.className = "block";
-     block.innerHTML = content;
-     block.setAttribute("onmousemove", "moveBlock(this);");
-     field.children[0].appendChild(block);
-}
-
 function hideShowBlockPanel(block) {
      if (block.name == "shown") {
           block.name = "hided";
@@ -28,4 +20,29 @@ function hideShowBlockPanel(block) {
           block.style.backgroundImage = "url(img/left-arrow.png)";
           block.parentElement.style.left = "0";
      }
+}
+
+function addBlock(content) {
+     var inputConnectors = '<button class="in-con con" onclick="connect(this);" name="no-con"></button>',
+         outputConnectors = '<button class="out-con con" onclick="connect(this);" name="no-con"></button>';
+     var block = document.createElement("button");
+
+     block.className = "block";
+     block.setAttribute("onmousemove", "moveBlock(this);");
+
+     if (content == 'input')
+          inputConnectors = "";
+     else if (content == 'output')
+          outputConnectors = "";
+     else if (content == 'isSubstring')
+          inputConnectors = inputConnectors.repeat(1);
+     else if (content == 'replace')
+          inputConnectors = inputConnectors.repeat(3);
+     else
+          inputConnectors = inputConnectors.repeat(2);
+
+     block.innerHTML = content + '<div class="input-connectors connectors">' + inputConnectors
+                     + '</div><div class="output-connectors connectors">' + outputConnectors
+                     + '</div>';
+     field.children[0].appendChild(block);
 }
--- irek-nazmiev/js/field-control-module.js
+++ irek-nazmiev/js/field-control-module.js
@@ -1,41 +1,46 @@
+var isBusy = false;
+
 function moveBlock(block) {
-     block.onmousedown = function(e) {       // start moving on first click
-          if (e.button == 0) {
-               var x, y;
+          block.onmousedown = function(e) {       // start moving on first click
+               if (!isBusy) {
+                    if (e.button == 0) {
+                         var x, y;
 
-               var blockCoords = block.getBoundingClientRect(),
-                   shiftX = e.pageX - blockCoords.left,
-                   shiftY = e.pageY - blockCoords.top;
+                         var blockCoords = block.getBoundingClientRect(),
+                             shiftX = e.pageX - blockCoords.left,
+                             shiftY = e.pageY - blockCoords.top;
 
-               block.parentElement.style.zIndex = 1000;
-               document.getElementById("trash-can-wrapper").style.left = 0;
+                         block.parentElement.style.zIndex = 1000;
+                         document.getElementById("trash-can-wrapper").style.left = 0;
 
-               document.onmousemove = function(e) {    // when mouse is moving, change block's coordinates
-                    var fieldMovableCoords = field.children[0].getBoundingClientRect();
-                    block.style.left = e.pageX - shiftX - fieldMovableCoords.left + 'px';
-                    block.style.top = e.pageY - shiftY - fieldMovableCoords.top + 'px';
-                    x = e.pageX;
-                    y = e.pageY;
-               };
+                         document.onmousemove = function(e) {    // when mouse is moving, change block's coordinates
+                                   var fieldMovableCoords = field.children[0].getBoundingClientRect();
+                                   block.style.left = e.pageX - shiftX - fieldMovableCoords.left + 'px';
+                                   block.style.top = e.pageY - shiftY - fieldMovableCoords.top + 'px';
 
-               block.onmouseup = function() {          // stop moving on second click
-                    document.onmousemove = null;
-                    block.onmouseup = null;
-                    block.parentElement.style.zIndex = "unset";
-                    if (isInTrashCan(x, y))
-                         block.remove();
-                    document.getElementById("trash-can-wrapper").style.left = "-20vw";
-               };
+                                   x = e.pageX;
+                                   y = e.pageY;
+                         };
 
-               function isInTrashCan(x, y){
-                    var trashCanCoords = document.getElementById("trash-can").getBoundingClientRect();
-                    return x >= trashCanCoords.left &
-                           x <= (trashCanCoords.left + trashCanCoords.width) &
-                           y >= trashCanCoords.top &
-                           y <= (trashCanCoords.top + trashCanCoords.height);
+                         block.onmouseup = function() {          // stop moving on second click
+                              document.onmousemove = null;
+                              block.onmouseup = null;
+                              block.parentElement.style.zIndex = "unset";
+                              if (isInTrashCan(x, y))
+                                   block.remove();
+                              document.getElementById("trash-can-wrapper").style.left = "-20vw";
+                         };
+
+                         function isInTrashCan(x, y){
+                              var trashCanCoords = document.getElementById("trash-can").getBoundingClientRect();
+                              return x >= trashCanCoords.left &
+                                     x <= (trashCanCoords.left + trashCanCoords.width) &
+                                     y >= trashCanCoords.top &
+                                     y <= (trashCanCoords.top + trashCanCoords.height);
+                         }
+                    }
                }
           }
-     }
 }
 
 function moveField(block) {
@@ -59,5 +64,84 @@ function moveField(block) {
 
      block.oncontextmenu = function(e) {
           return false;
+     }
+}
+
+function createLineElement(x1, y1, x2, y2) {
+     var line = document.createElement("div");
+     var lineData = createLine(x1, y1, x2, y2);
+     var styles = 'width: ' + lineData.length + 'px; '
+                + 'transform: rotate(' + lineData.angle + 'rad); '
+                + 'top: ' + lineData.y + 'px; '
+                + 'left: ' + lineData.x + 'px;';
+     line.setAttribute('style', styles);
+     line.setAttribute('class', 'line');
+     return line;
+}
+
+function createLine(x1, y1, x2, y2) {
+     var a = x1 - x2,
+         b = y1 - y2,
+         length = Math.sqrt(a * a + b * b);
+
+     var sx = (x1 + x2) / 2,
+         sy = (y1 + y2) / 2;
+
+     var x = sx - length / 2,
+         y = sy;
+
+     var angle = Math.PI - Math.atan2(-b, a);
+
+     return {
+          x: x,
+          y: y,
+          length: length,
+          angle: angle
+     };
+}
+
+function updateLineElement(line, x1, y1, x2, y2) {
+     var lineData = createLine(x1, y1, x2, y2);
+     var styles = 'width: ' + lineData.length + 'px; '
+                + 'transform: rotate(' + lineData.angle + 'rad); '
+                + 'top: ' + lineData.y + 'px; '
+                + 'left: ' + lineData.x + 'px;';
+     line.setAttribute('style', styles);
+}
+
+function connect(connector) {
+     if (connector.name == 'no-con' && !isBusy) {
+          isBusy = true;
+          var conCoords = connector.getBoundingClientRect(),
+              fieldMovableCoords = field.children[0].getBoundingClientRect(),
+              x1 = conCoords.x - fieldMovableCoords.x + 5,
+              y1 = conCoords.y - fieldMovableCoords.y + 5,
+              line = createLineElement(x1, y1, x1, y1);
+          field.children[0].appendChild(line);
+
+          document.onmousemove = function(e) {
+               var fieldMovableCoords = field.children[0].getBoundingClientRect(),
+                   x2 = e.pageX - fieldMovableCoords.x,
+                   y2 = e.pageY - fieldMovableCoords.y;
+
+               updateLineElement(line, x1, y1, x2, y2);
+
+               document.onclick = function(e) {
+                    if (e.target.name == 'no-con' && (connector.className != e.target.className) && (connector.parentElement.parentElement != e.target.parentElement.parentElement)) {
+                         e.target.name = 'con';
+                         connector.name = 'con';
+                         document.onmousemove = null;
+                         document.onclick = null;
+                         isBusy = false;
+                    }
+               }
+
+               document.oncontextmenu = function(e) {
+                    document.onmousemove = null;
+                    document.onclick = null;
+                    line.remove();
+                    isBusy = false;
+               }
+          };
      }
 }
